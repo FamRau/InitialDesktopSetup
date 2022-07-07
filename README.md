913fcda25adbd7fe28dea5795dc91117bde90c68
Gerhard Rauniak <gerhard.rauniak@gmail.com>
2022-07-07 21:14:39 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# InitialDesktopSetup
+# Initial Desktop Setup
+
+
+This script is used to set up a new Linux installation with my programs and settings.
+
+
+USE:
+        sudo ./initial_setup.sh
+
+TODO:
+- [x] Write a function to check if a program is installed. If not, ask user if it should be installed.
+- [ ] Extend the call block to all programs that should be installed.
+- [ ] Copy the settings files from an archive into the new home folder.
---
+++ initial_setup.sh
@@ -0,0 +1,54 @@
+#! /usr/bin/bash
+
+########## Initial Desktop Setup ########################
+# Script to setup a new computer
+# by Gerhard Rauniak
+# Juli 2022
+# 
+# README.md https://github.com/FamRau/InitialDesktopSetup
+#########################################################
+
+
+use: sudo ./chk_install
+
+# color definitions
+green="\033[0;32m"
+red="\033[0;31m"
+blank="\033[0m"
+
+
+
+#### FUNCTION BEGIN
+# Checks if program is installed
+# If not, user is asked wether to install it or not
+# ARGUMENT:
+#       Name of the program to be checked
+# OUTPUT:
+#       Install the program if it's not installed
+#       It installed, end the script.        
+### FUNCTION END
+check_file () {
+if ! command -v $1 &> /dev/null
+then
+    echo -e "$red*** $1 ***$blank could not be found. Do you want to install it y/n?"
+    read answer 
+    while [ $answer != "y" ] && [ $answer != "n" ] #check if user input is y or no 
+    do
+        echo "Only y or n!"
+        read answer
+    done
+
+    if [ $answer == "y" ]
+    then
+        apt install $1  
+        echo -e "$green*** $1 ***$blank successfully installed"
+    fi
+
+else
+    echo -e "$green*** $1 ***$blank is already installed."
+    exit
+fi
+}
+
+# calls check_file function
+check_file "cronic" # cronic is only used for testing
